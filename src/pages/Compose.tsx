1b324e32a78e8b1fb881a63dafb783ea0deb5658
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-05-19 01:28:51 +0800

    Add part of Compose page UI

--- src/pages/Compose.tsx
+++ src/pages/Compose.tsx
@@ -1,7 +1,54 @@
+import { Button, Group, Menu, SimpleGrid, Text, Title } from "@mantine/core";
 import React from "react";
+import {
+  HiOutlineDocumentDuplicate,
+  HiOutlinePaperAirplane,
+  HiOutlinePencilSquare,
+} from "react-icons/hi2";
+import { SiGmail, SiMicrosoftoutlook } from "react-icons/si";
 
 const Compose: React.FC = () => {
-  return <div>Compose</div>;
+  return (
+    <div>
+      <SimpleGrid cols={2}>
+        <div>
+          <Title order={2}>
+            <Group>
+              <HiOutlinePencilSquare /> Let's create an email!
+            </Group>
+          </Title>
+          <Text>Let's streamline the process of creating your message.</Text>
+          <Text>
+            Simply type your message below. Focus on the content – don't worry
+            about styling, we will theme it. You can utilize Markdown to add
+            formatting and structure to your message
+          </Text>
+        </div>
+        <div>
+          <Menu>
+            <Menu.Target>
+              <Button rightIcon={<HiOutlinePaperAirplane />}>
+                Send Message
+              </Button>
+            </Menu.Target>
+
+            <Menu.Dropdown>
+              <Menu.Label>Send via</Menu.Label>
+              <Menu.Item icon={<SiMicrosoftoutlook color="#0078D4" />}>
+                Outlook
+              </Menu.Item>
+              <Menu.Item icon={<SiGmail color="#EA4335" />}>Gmail</Menu.Item>
+              <Menu.Divider />
+              <Menu.Label>Alternatively</Menu.Label>
+              <Menu.Item icon={<HiOutlineDocumentDuplicate />}>
+                Copy to clipboard
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+        </div>
+      </SimpleGrid>
+    </div>
+  );
 };
 
 export default Compose;
