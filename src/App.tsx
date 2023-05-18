20bb8709220b20e0d78faef04771daaa6c1875a3
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-05-18 23:39:22 +0800

    Use Mantine UI in app
    Also removed some CSS.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { MantineProvider, Title } from "@mantine/core";
 import React from "react";
 
 const App: React.FC = () => {
-  return <h1>glam-mailer</h1>;
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Title>glam-mailer</Title>
+    </MantineProvider>
+  );
 };
 
 export default App;
