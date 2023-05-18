acf4957fe81aa266334a1ee2955967d30cc5ae08
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-05-19 05:10:08 +0800

    Fix Tailwind classes not loading

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
+// Comment out in development to save on compile time
+// const safelist = [/.+/];
+const safelist = [/bg-.+/, /font-.+/, /text-.+/];
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,yaml}"],
   theme: {
-    extend: {},
+    extend: { ...defaultTheme },
   },
   plugins: [],
   prefix: "tw-",
+  safelist: safelist.map((regex) => ({ pattern: regex })),
 };
