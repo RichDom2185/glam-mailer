d18b07028de54e04d7d1957c02abe58917a9b418
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-05-20 01:40:28 +0800

    Use environment variables for configurations

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,11 @@
 // TODO: Use environment variables instead of hardcoding
-export const BACKEND_URL = "http://localhost:8080";
+export const BACKEND_URL: string =
+  import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
 
 export const API_CONVERSION_ENDPOINT_URL = BACKEND_URL + "/convert";
 
-export const TAILWIND_CLASS_PREFIX = "tw-";
+export const TAILWIND_CLASS_PREFIX: string =
+  import.meta.env.VITE_TAILWIND_PREFIX ?? "tw-";
 
 // TODO: Remove and replace this with a real email message
 export const PLACEHOLDER_MARKDOWN_CONTENT = `
