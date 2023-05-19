748469af502228cff8308fa55f41c071222f90bb
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-05-19 23:46:28 +0800

    Use backend for html conversion (#2)
    * Add copies of Tailwind CSS files
    
    * Install Axios
    
    * Use backend for HTML conversion
    
    Uses our backend service to convert the HTML from a standard format to
    an email-compatible format. This includes inlining all the CSS styles,
    as well as replacing CSS variables lookups with their values, and
    removing any remaining unused styles.

---
+++ src/utils/constants.ts
@@ -0,0 +1,4 @@
+// TODO: Use environment variables instead of hardcoding
+export const BACKEND_URL = "http://localhost:8080";
+
+export const API_CONVERSION_ENDPOINT_URL = BACKEND_URL + "/convert";
--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,6 +1,11 @@
 import { Components } from "react-markdown";
 import { parse } from "yaml";
+import components from "../themes/components.min.css?raw";
 import defaultTheme from "../themes/default.yaml?raw";
+import utilities from "../themes/utilities.min.css?raw";
+
+import axios from "axios";
+import { API_CONVERSION_ENDPOINT_URL } from "./constants";
 
 export type Theme = {
   name: string;
@@ -11,6 +16,15 @@ export type Theme = {
   };
 };
 
+// TODO: Use redux
 export const getTheme = () => {
   return parse(defaultTheme);
+};
+
+export const formatAsHtmlEmail = async (html: string) => {
+  const tailwind = "<style>\n" + components + "\n" + utilities + "\n</style>\n";
+  const res = await axios.post(API_CONVERSION_ENDPOINT_URL, {
+    html: tailwind + html,
+  });
+  return res.data;
 };
