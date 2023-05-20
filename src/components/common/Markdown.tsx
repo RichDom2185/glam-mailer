c33ee8d44855789c9e9c17dbc4474a0186f18472
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-05-20 19:25:40 +0800

    Refactor class mapping logic
    * Migrate class mapping logic under theme utilities
    * Add TODOs where applicable

--- src/components/common/Markdown.tsx
+++ src/components/common/Markdown.tsx
@@ -1,10 +1,9 @@
 import React, { useMemo } from "react";
-import { Components } from "react-markdown";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+// TODO: Set up type definitions
 import addClasses from "rehype-add-classes";
 import remarkGfm from "remark-gfm";
-import { TAILWIND_CLASS_PREFIX } from "../../utils/constants";
-import { Theme, getTheme } from "../../utils/theme";
+import { getClassMappingFrom, getTheme } from "../../utils/theme";
 
 type Props = {
   containerRef?: React.RefObject<HTMLDivElement>;
@@ -15,27 +14,13 @@ const plugins = [remarkGfm];
 
 const Markdown: React.FC<Props> = ({ containerRef, children }) => {
   // TODO: Use a prop to respect abstraction boundaries
-  const { styles } = getTheme() as Theme;
-  // Must use any here because of the large union type
-
+  const theme = getTheme();
   const classesToAdd = useMemo(() => {
     const classesToAdd: {
       [key: string]: string;
-    } = {};
-    Object.keys(styles).forEach((tag) => {
-      // Must use any here because of the large union type
-      const tailwindClasses = styles[tag as keyof Components] ?? [];
-      classesToAdd[tag] = tailwindClasses
-        .flatMap((style) => [
-          // We include the default style as well
-          // for use when exporting as email
-          style,
-          `${TAILWIND_CLASS_PREFIX}${style}`,
-        ])
-        .join(" ");
-    });
+    } = getClassMappingFrom(theme);
     return classesToAdd;
-  }, [styles]);
+  }, [theme]);
 
   return (
     <div ref={containerRef}>
