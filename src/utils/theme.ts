dd718a60eb802327e8e08facb9a85a2f307f3bf2
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-05-20 20:30:20 +0800

    Support CSS selectors in theme files (#5)
    * Install rehype-add-classes
    
    * Redo rendering logic to use rehype-add-classes plugin
    
    As CSS selectors are a superset of HTML tags, we can safely remove our
    custom component handler in favour of simply using the plugin for
    all the injections of the Tailwind classes.
    
    * Refactor class mapping logic
    
    * Migrate class mapping logic under theme utilities
    * Add TODOs where applicable
    
    * Support more CSS selectors in theme files
    
    See https://github.com/martypdx/rehype-add-classes for more info. The
    rehype-add-classes plugin uses a very old version of hast-util-select as
    a transitive dependency, so we manually bump the version in our
    package.json using selective dependency resolution in order to support
    additional CSS selectors like `:has()`.

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -3,20 +3,42 @@ import { parse } from "yaml";
 import defaultTheme from "../themes/default.yaml?raw";
 
 import axios from "axios";
-import { API_CONVERSION_ENDPOINT_URL } from "./constants";
+import {
+  API_CONVERSION_ENDPOINT_URL,
+  TAILWIND_CLASS_PREFIX,
+} from "./constants";
 
 export type Theme = {
   name: string;
   author: string;
   description: string;
   styles: {
+    // TODO: Update types to reflect usage of CSS selectors,
+    //       not just HTML element tags
     [tag in keyof Components]: string[];
   };
 };
 
 // TODO: Use redux
-export const getTheme = () => {
-  return parse(defaultTheme);
+export const getTheme = (): Theme => {
+  return parse(defaultTheme) as Theme;
+};
+
+export const getClassMappingFrom = (theme: Theme): Record<string, string> => {
+  const { styles } = theme;
+  const classMap: Record<string, string> = {};
+  Object.keys(styles).forEach((tag) => {
+    // Must use any here because of the large union type
+    // TODO: Fix typings to reflect usage of CSS selectors
+    const tailwindClasses = styles[tag as keyof Components] ?? [];
+    classMap[tag] = tailwindClasses
+      // We include the default style as well
+      // for use when exporting as email
+      // TODO: Use prefix for exports
+      .flatMap((style) => [style, `${TAILWIND_CLASS_PREFIX}${style}`])
+      .join(" ");
+  });
+  return classMap;
 };
 
 export const formatAsHtmlEmail = async (html: string) => {
