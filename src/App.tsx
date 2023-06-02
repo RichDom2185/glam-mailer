01c203c7989c2337d7edd6b6e7944f0667b8d994
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-06-02 22:41:15 +0800

    Pre-warm backend on app load
    This somewhat overcomes free tier limitations of the backend spinning
    down after a period of inactivity.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { AppShell, MantineProvider } from "@mantine/core";
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
+import { sendHello } from "./api/general";
 import AppHeader from "./components/app/AppHeader";
 import AppNavigation from "./components/app/AppNavigation";
 import Compose from "./pages/Compose";
 import Drafts from "./pages/Drafts";
 
 const App: React.FC = () => {
+  useEffect(() => {
+    sendHello();
+  }, []);
+
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <AppShell
