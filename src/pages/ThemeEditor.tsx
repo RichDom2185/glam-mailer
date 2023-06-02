a6956ab4091f5836a0fc5346f4ae8f55d61408a1
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-06-02 23:45:28 +0800

    Create UI for Theme Editor page

--- src/pages/ThemeEditor.tsx
+++ src/pages/ThemeEditor.tsx
@@ -1,7 +1,87 @@
-import React from "react";
+import {
+  Box,
+  Button,
+  Divider,
+  Group,
+  SimpleGrid,
+  Space,
+  Text,
+  Title,
+} from "@mantine/core";
+import React, { useState } from "react";
+import AceEditor from "react-ace";
+import {
+  HiOutlineArrowPath,
+  HiOutlineBackspace,
+  HiOutlinePaintBrush,
+} from "react-icons/hi2";
+import SAMPLE_MARKDOWN_CONTENT from "../assets/md-sample.md?raw";
+import Markdown from "../components/common/Markdown";
+import defaultTheme from "../themes/default.yaml?raw";
+
+import "ace-builds/src-noconflict/ext-language_tools";
+import "ace-builds/src-noconflict/mode-yaml";
 
 const ThemeEditor: React.FC = () => {
-  return <div>ThemeEditor</div>;
+  const [editorValue, setEditorValue] = useState(defaultTheme);
+
+  return (
+    <div>
+      <SimpleGrid cols={2}>
+        <div>
+          <Title order={2}>
+            <Group>
+              <HiOutlinePaintBrush />
+              Let us create a theme!
+            </Group>
+          </Title>
+          <Space h="md" />
+          <Text>
+            Start by writing your theme following the YAML template below. The
+            default theme has been loaded for you. Feel free to customize it, or
+            start from a blank slate; your theme will be visible on the right.
+          </Text>
+          <Space h="md" />
+          <Text>
+            Documentation for building theme files will be coming soon.
+          </Text>
+          <Space h="md" />
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Button
+              rightIcon={<HiOutlineArrowPath />}
+              onClick={() => setEditorValue(defaultTheme)}
+            >
+              Reset to Default
+            </Button>
+            <Button
+              color="red"
+              rightIcon={<HiOutlineBackspace />}
+              onClick={() => setEditorValue("")}
+            >
+              Clear All
+            </Button>
+          </Box>
+          <Divider
+            my="sm"
+            variant="dashed"
+            label="Write your theme below"
+            labelPosition="center"
+          />
+          <AceEditor
+            mode="yaml"
+            width="100%"
+            onChange={setEditorValue}
+            value={editorValue}
+            wrapEnabled
+          />
+        </div>
+        <div>
+          {/* TODO: Load every possible supported (extended) Markdown construct */}
+          <Markdown>{SAMPLE_MARKDOWN_CONTENT}</Markdown>
+        </div>
+      </SimpleGrid>
+    </div>
+  );
 };
 
 export default ThemeEditor;
