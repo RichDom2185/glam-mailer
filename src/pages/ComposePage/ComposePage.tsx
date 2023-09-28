d0ca275c804e5bc623c37285d0043c74118ad785
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-09-28 22:45:22 +0800

    Split ComposePage to multiple components
    * Removed unnecessary components
    * Wrap header in Card component
    * Remove unnecessary header text
    * Refactor handlers using dependency injection

--- src/pages/ComposePage/ComposePage.tsx
+++ src/pages/ComposePage/ComposePage.tsx
@@ -1,34 +1,23 @@
 import {
-  Box,
   Button,
   Divider,
   Group,
-  Menu,
   Notification,
   SimpleGrid,
   Space,
-  Text,
-  Title,
 } from "@mantine/core";
-import React, { useCallback, useRef, useState } from "react";
-import {
-  HiOutlineArrowTopRightOnSquare,
-  HiOutlineBackspace,
-  HiOutlineDocumentDuplicate,
-  HiOutlineDocumentText,
-  HiOutlinePaperAirplane,
-  HiOutlinePencilSquare,
-} from "react-icons/hi2";
-import ReactToPrint from "react-to-print";
+import React, { useRef, useState } from "react";
+import { HiOutlineBackspace, HiOutlineDocumentText } from "react-icons/hi2";
 import SAMPLE_MARKDOWN_CONTENT from "../../assets/md-sample.md?raw";
 import Editor from "../../components/common/Editor";
 import Markdown from "../../components/common/Markdown";
 import { PLACEHOLDER_MARKDOWN_CONTENT } from "../../utils/constants";
-import { MAIL_PROVIDERS } from "../../utils/mail";
-import { formatAsHtmlEmail, getTheme } from "../../utils/theme";
+import { getTheme } from "../../utils/theme";
 
 import "ace-builds/src-noconflict/ext-language_tools";
 import "ace-builds/src-noconflict/mode-markdown";
+import ComposePageHeader from "./ComposePageHeader";
+import ComposePageMenu from "./ComposePageMenu";
 
 const ComposePage: React.FC = () => {
   const [editorValue, setEditorValue] = useState(PLACEHOLDER_MARKDOWN_CONTENT);
@@ -37,54 +26,12 @@ const ComposePage: React.FC = () => {
   const [isFormatting, setIsFormatting] = useState(false);
 
   const theme = getTheme();
-
-  const handleCopyToClipboard = useCallback(async () => {
-    const e = ref.current;
-    if (!e) {
-      return;
-    }
-
-    setIsFormatting(true);
-    const html = e.innerHTML;
-    const formattedHtml = await formatAsHtmlEmail(html);
-    navigator.clipboard
-      .write([
-        new ClipboardItem({
-          "text/html": new Blob([formattedHtml], { type: "text/html" }),
-          "text/plain": new Blob([formattedHtml], { type: "text/plain" }),
-        }),
-      ])
-      .then(() => {
-        setIsFormatting(false);
-        alert("Copied to clipboard!");
-      })
-      .catch(() => {
-        setIsFormatting(false);
-        alert("Something went wrong.");
-      });
-  }, [ref]);
-
   return (
     <div>
       <SimpleGrid cols={2}>
         <div>
-          <Title order={2}>
-            <Group>
-              <HiOutlinePencilSquare /> Let's create an email!
-            </Group>
-          </Title>
-          <Space h="md" />
-          <Text>Let's streamline the process of creating your message.</Text>
-          <Space h="md" />
-          <Text>
-            Simply type your message below. Focus on the content – don't worry
-            about styling, we will theme it. You can utilize Markdown to add
-            formatting and structure to your message.
-          </Text>
-          <Space h="md" />
-          <Text>Unsure of what's supported? Load the example below!</Text>
-          <Space h="md" />
-          <Box style={{ display: "flex", justifyContent: "space-between" }}>
+          <ComposePageHeader />
+          <Group mt="md" justify="space-between">
             <Button
               rightSection={<HiOutlineDocumentText />}
               onClick={() => setEditorValue(SAMPLE_MARKDOWN_CONTENT)}
@@ -98,7 +45,7 @@ const ComposePage: React.FC = () => {
             >
               Clear All
             </Button>
-          </Box>
+          </Group>
           <Divider
             my="sm"
             variant="dashed"
@@ -112,48 +59,7 @@ const ComposePage: React.FC = () => {
           />
         </div>
         <div>
-          <Menu>
-            <Menu.Target>
-              <Button rightSection={<HiOutlinePaperAirplane />}>
-                Send Message
-              </Button>
-            </Menu.Target>
-
-            <Menu.Dropdown>
-              <Menu.Label>Send via (coming soon)</Menu.Label>
-              {MAIL_PROVIDERS.map((provider) => {
-                const ProviderIcon = provider.icon;
-                return (
-                  <Menu.Item
-                    disabled // TODO: Remove when ready
-                    key={provider.label}
-                    leftSection={
-                      <ProviderIcon color={false && provider.color} />
-                    }
-                  >
-                    {provider.label}
-                  </Menu.Item>
-                );
-              })}
-              <Menu.Divider />
-              <Menu.Label>Manual send</Menu.Label>
-              <Menu.Item
-                leftSection={<HiOutlineDocumentDuplicate />}
-                onClick={() => handleCopyToClipboard()}
-              >
-                Copy to clipboard
-              </Menu.Item>
-              <ReactToPrint
-                pageStyle=""
-                trigger={() => (
-                  <Menu.Item leftSection={<HiOutlineArrowTopRightOnSquare />}>
-                    Save as PDF
-                  </Menu.Item>
-                )}
-                content={() => ref.current}
-              />
-            </Menu.Dropdown>
-          </Menu>
+          <ComposePageMenu sourceRef={ref} loadingCallback={setIsFormatting} />
           {isFormatting && (
             <>
               <Space h="md" />
---
+++ src/pages/ComposePage/ComposePageMenu.tsx
@@ -0,0 +1,87 @@
+import { Button, Menu } from "@mantine/core";
+import React from "react";
+import {
+  HiOutlineArrowTopRightOnSquare,
+  HiOutlineDocumentDuplicate,
+  HiOutlinePaperAirplane,
+} from "react-icons/hi2";
+import ReactToPrint from "react-to-print";
+import { MAIL_PROVIDERS } from "../../utils/mail.ts";
+import { formatAsHtmlEmail } from "../../utils/theme.ts";
+
+const handleCopyToClipboard = async (
+  sourceRef: React.RefObject<HTMLDivElement>,
+  loadingCallback: (loadingState: boolean) => void = () => {}
+) => {
+  const e = sourceRef.current;
+  if (!e) {
+    return;
+  }
+
+  loadingCallback(true);
+  const html = e.innerHTML;
+  const formattedHtml = await formatAsHtmlEmail(html);
+  navigator.clipboard
+    .write([
+      new ClipboardItem({
+        "text/html": new Blob([formattedHtml], { type: "text/html" }),
+        "text/plain": new Blob([formattedHtml], { type: "text/plain" }),
+      }),
+    ])
+    .then(() => alert("Copied to clipboard!"))
+    .catch((e) => {
+      alert("Something went wrong.");
+      console.error(e);
+    })
+    .finally(() => loadingCallback(false));
+};
+
+type Props = {
+  sourceRef: React.RefObject<HTMLDivElement>;
+  loadingCallback?: (loadingState: boolean) => void;
+};
+
+const ComposePageMenu: React.FC<Props> = ({ sourceRef, loadingCallback }) => {
+  return (
+    <Menu>
+      <Menu.Target>
+        <Button rightSection={<HiOutlinePaperAirplane />}>Send Message</Button>
+      </Menu.Target>
+
+      <Menu.Dropdown>
+        <Menu.Label>Send via (coming soon)</Menu.Label>
+        {MAIL_PROVIDERS.map((provider) => {
+          const ProviderIcon = provider.icon;
+          return (
+            <Menu.Item
+              disabled // TODO: Remove when ready
+              key={provider.label}
+              leftSection={<ProviderIcon color={provider.color} />}
+            >
+              {provider.label}
+            </Menu.Item>
+          );
+        })}
+        <Menu.Divider />
+        <Menu.Label>Manual send</Menu.Label>
+        <Menu.Item
+          leftSection={<HiOutlineDocumentDuplicate />}
+          onClick={() => handleCopyToClipboard(sourceRef, loadingCallback)}
+        >
+          Copy to clipboard
+        </Menu.Item>
+        <ReactToPrint
+          pageStyle=""
+          trigger={() => (
+            <Menu.Item leftSection={<HiOutlineArrowTopRightOnSquare />}>
+              Save as PDF
+            </Menu.Item>
+          )}
+          content={() => sourceRef.current}
+        />
+      </Menu.Dropdown>
+    </Menu>
+  );
+};
+
+export default ComposePageMenu;
