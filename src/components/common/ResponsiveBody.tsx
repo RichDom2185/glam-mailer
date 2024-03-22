1a8308ec0fd4a49f965390e93b7a090ea8b4fa20
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-03-22 12:03:56 +0800

    Create ResponsiveBody component

---
+++ src/components/common/ResponsiveBody.tsx
@@ -0,0 +1,56 @@
+import { SegmentedControl, SimpleGrid, Tabs } from "@mantine/core";
+import React, { useState } from "react";
+
+type Props = {
+  data: ReadonlyArray<{
+    element: React.ReactNode;
+    label: React.ReactNode;
+  }>;
+};
+
+const ResponsiveBody: React.FC<Props> = ({ data }) => {
+  const [activeTab, setActiveTab] = useState("0");
+  return (
+    <>
+      <SimpleGrid cols={data.length} visibleFrom="md">
+        {data.map(({ element }, index) => (
+          <React.Fragment key={index}>{element}</React.Fragment>
+        ))}
+      </SimpleGrid>
+      <Tabs
+        hiddenFrom="md"
+        display="flex"
+        style={{ flexDirection: "column" }}
+        h="85vh"
+        value={activeTab}
+      >
+        <SegmentedControl
+          pb={8} // Hardcoding to fix spacing bug
+          mb="xs"
+          fullWidth
+          data={data.map(({ label }, i) => ({ label, value: `${i}` }))}
+          onChange={setActiveTab}
+        />
+        {/* <Tabs.List grow>
+          {data.map(({ label }, index) => (
+            <Tabs.Tab key={index} value={`${index}`}>
+              {label}
+            </Tabs.Tab>
+          ))}
+        </Tabs.List> */}
+        {data.map(({ element }, index) => (
+          <Tabs.Panel
+            h="100%"
+            key={index}
+            value={`${index}`}
+            style={{ flexGrow: 1 }}
+          >
+            {element}
+          </Tabs.Panel>
+        ))}
+      </Tabs>
+    </>
+  );
+};
+
+export default ResponsiveBody;
