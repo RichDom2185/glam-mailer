c0742b224268a6228471f79c4c1034f4df7b71e9
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-06-22 16:17:03 +0800

    Scaffold app state layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import { appRoutes } from "./routes.ts";
+import { store } from "./store/index.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider
-      router={createBrowserRouter(
-        [{ path: "/*", element: <App />, children: appRoutes }],
-        { future: { v7_relativeSplatPath: true } }
-      )}
-    />
+    <Provider store={store}>
+      <RouterProvider
+        router={createBrowserRouter(
+          [{ path: "/*", element: <App />, children: appRoutes }],
+          { future: { v7_relativeSplatPath: true } }
+        )}
+      />
+    </Provider>
   </React.StrictMode>
 );
