dbe79a5e5cc71ff5ec8e75b84daefed4476163ba
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-08-17 19:14:31 +0800

    Update draft type

--- src/types/draft.ts
+++ src/types/draft.ts
@@ -2,5 +2,8 @@ export type Draft = {
   id: string;
   // TODO: Set up themeId when slice is introduced
   // themeId: string;
+  subject: string;
   content: string;
+  /** ISO-string of created timestamp */
+  createdAt: string;
 };
