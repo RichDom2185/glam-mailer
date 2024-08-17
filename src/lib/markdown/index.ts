6ae99a62becb80c94aa1971f30b46304cb2da1e4
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-08-17 17:54:33 +0800

    Update library index to include new templates
    This was missed in 0c2c4bf843baffa2043066609a2ca929320029ea.

--- src/lib/markdown/index.ts
+++ src/lib/markdown/index.ts
@@ -1,7 +1,11 @@
+import appFeatures from "./app_features.md?raw";
 import defaultPlaceholder from "./placeholder.md?raw";
+import syntaxOverview from "./syntax_overview.md?raw";
 
 const markdownTemplates = Object.freeze({
+  appFeatures,
   defaultPlaceholder,
+  syntaxOverview,
 });
 
 export default markdownTemplates;
