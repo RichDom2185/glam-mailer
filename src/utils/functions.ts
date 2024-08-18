f320f57bf6c8ef721bbb0f6934ca3a7184b20c70
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-08-18 13:37:07 +0800

    Fix bugs and improve function signature

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,24 +1,44 @@
 import { throttle } from "es-toolkit";
 
 /**
- * Debounce function with memoization. Returns the last cached value if
- * the function is called again before the delay.
- * @param fn The function to be debounced and memoized.
- * @param delay The delay in milliseconds for debouncing.
- * @returns A debounced function with memoization.
+ * Throttles a function and caches its return value based on the
+ * function arguments. If the function is called with the same
+ * arguments before the delay has passed, the cached value is
+ * returned. If the function is called with different arguments
+ * before the delay has passed, either a fallback value, or the
+ * previous value is returned.
+ *
+ * @param fn          The function to be throttled.
+ * @param delay       The throttle delay in milliseconds.
+ * @param fallback    The fallback value to return if the function is called
+ *                    with different arguments before the delay has passed.
+ * @param getCacheKey A function that generates a cache key based on
+ *                    the function arguments.
+ * @returns           The throttled function.
  */
-export const throttleWithMemoize = <F extends (...args: any[]) => any>(
+export const throttleWithMemoize = <
+  F extends (...args: any[]) => any,
+  T extends ReturnType<F> = ReturnType<F>
+>(
   fn: F,
-  delay: number
+  delay: number,
+  fallback?: T,
+  getCacheKey?: (...args: Parameters<F>) => string
 ) => {
-  let cached: ReturnType<F>;
+  const cache: Record<string, ReturnType<F>> = {};
+
   const throttled = throttle((...args: Parameters<F>) => {
-    cached = fn(...args);
-    return cached;
+    const key = getCacheKey?.(...args) ?? args.join(",");
+    cache[key] = fn(...args);
+    return cache[key];
   }, delay);
 
   return (...args: Parameters<F>): ReturnType<F> => {
+    const key = getCacheKey?.(...args) ?? args.join(",");
+    if (key in cache) {
+      return cache[key];
+    }
     throttled(...args);
-    return cached;
+    return cache[key] ?? (fallback as ReturnType<F>);
   };
 };
